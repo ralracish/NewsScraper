47f1aaad8b5902eb3297ec1e8ca85e7de200765f
ralracish <carlarblackman@gmail.com>
2019-05-21 18:12:30 -0400

    option for PORT on heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cheerio = require("cheerio");
 // Require all models
 const db = require("./models");
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
 // Initialize Express
 const app = express();
